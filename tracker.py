247d0b4597ba6745b008ea65716b08e58bb45052
SHUNYA007 <prashantsingh7@protonmail.com>
2021-07-03 21:57:11 +0530

    fixed spacing and comments are included

--- tracker.py
+++ tracker.py
@@ -90,14 +90,25 @@ class Tracker(object):
             list_peers = bdecode(answer_tracker.content)
             offset=0
             if not type(list_peers['peers']) == dict:
+                '''
+                    - Handles bytes form of list of peers
+                    - IP address in bytes form:
+                        - Size of each IP: 6 bytes
+                        - The first 4 bytes are for IP address
+                        - Next 2 bytes are for port number
+                    - To unpack initial 4 bytes !i (big-endian, 4 bytes) is used.
+                    - To unpack next 2 byets !H(big-endian, 2 bytes) is used.
+                '''
                     for _ in range(len(list_peers['peers'])//6):
-                        ip  =   struct.unpack_from("!i", list_peers['peers'], offset)[0]
-                        ip =   socket.inet_ntoa(struct.pack("!i", ip))
-                        offset  +=  4
-                        port   =   struct.unpack_from("!H",list_peers['peers'], offset)[0]
-                        offset  +=  2
-                        s=SockAddr(ip,port)
-                        self.dict_sock_addr[s.__hash__()]=s
+                        
+                        ip = struct.unpack_from("!i", list_peers['peers'], offset)[0]
+                        ip = socket.inet_ntoa(struct.pack("!i", ip))
+                        #ntoa= numbers to stirng
+                        offset += 4
+                        port = struct.unpack_from("!H",list_peers['peers'], offset)[0]
+                        offset += 2
+                        s = SockAddr(ip,port)
+                        self.dict_sock_addr[s.__hash__()] = s
             else:
                 for p in list_peers['peers']:
                     s = SockAddr(p['ip'], p['port'])
